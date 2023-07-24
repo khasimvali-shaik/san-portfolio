edb7fe0c01377ae31b9b76f9c50203113df26208
shaik khasim vali <ShaikKhasim.Vali@brillio.com>
2023-07-24 16:03:17 +0530

    added about page

---
+++ dist/js/about.js
@@ -0,0 +1,38 @@
+const navbar = document.getElementById("main-nav");
+window.onscroll = () => {
+  if (window.pageYOffset > 200) {
+    navbar.classList.remove("top");
+  } else {
+    navbar.classList.add("top");
+  }
+};
+
+function goToHome() {
+  const currentPageURL = window.location.pathname;
+
+  if (currentPageURL.endsWith("index.html")) {
+    window.scrollTo(0, 0);
+  } else {
+    window.location.href = "index.html";
+  }
+}
+
+function toggleParagraphs(event) {
+  event.preventDefault();
+
+  // Get all the paragraphs in the container
+  const allParagraphs = document.querySelectorAll(".column-2 p");
+  const readIcon = document.getElementById("read-more");
+
+  // Toggle the "hidden" class for each paragraph except the first three
+  for (let i = 3; i < allParagraphs.length; i++) {
+    allParagraphs[i].classList.toggle("hidden");
+  }
+
+  // Update the text of the "Read More" button
+  const readMoreButton = event.target;
+  const isExpanded = readMoreButton.classList.toggle("expanded");
+  readMoreButton.innerHTML = `<i class="fas fa-chevron-${
+    isExpanded ? "up" : "down"
+  }"></i> ${isExpanded ? "Read Less" : "Read More"}`;
+}
